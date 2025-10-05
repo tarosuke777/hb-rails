f96d17c6d27503bdf90d5f181740f252921671b6
tarosuke <felis.silvestris.catus.7777@gmail.com>
2025-10-06 06:59:16 +0900

    投稿一覧の表示件数を12件に変更し、作成日時でソートするように修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     # @posts = Post.all
-    @posts = Post.page(params[:page]).per(3) # 1ページあたり10件表示
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(12)
   end
 
   # GET /posts/1 or /posts/1.json
